bde13e703dad98f4078532d3d772025cf37ffe45
Szilniczky <szilniczky.david@gmail.com>
2017-01-10 15:05:46 +0100

    connect done

--- sample-image-generator.py
+++ sample-image-generator.py
@@ -1,3 +1,13 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='szilniczky' user='szilniczky' host='localhost' password='Szeder93'")
+except:
+    print("I am unable to connect to the database")
+cur = conn.cursor()
+cur.execute("""SELECT name FROM base_data WHERE name='Y-find'""")
+text_content=cur.fetchall()[0]
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -6,10 +16,7 @@ img = Image.new("RGB", (512, 512), "red")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
 # font = ImageFont.truetype("sans-serif.ttf", 16)
-text_options = {
-    'fill': (255, 255, 255)
-}
-text_content = "Sample Text"
+text_options = {'fill': (255, 255, 255)}
 text_size = draw.textsize(text_content)
 # draw.text((x, y),text_content,(r,g,b))
 draw.text((0, 0), text_content, **text_options)
