f95019ac97ce8aab2881060532339b39ab9ede54
lxndrvn <i1990522@gmail.com>
2017-01-11 15:18:24 +0100

    database connection

--- db_connection.py
+++ db_connection.py
@@ -1,28 +1,36 @@
 import psycopg2
 
-def DbConnect():
+class DbConnect():
     def __init__(self, db_name, username, host_name, password):
         self.db_name = db_name
         self.username = username
         self.host_name = host_name
         self.password = password
+        self.conn_str = "dbname='" + self.db_name + "' user='" + self.username + "' host='localhost' password='"+ self.password +"'"
+        self.connection = None
     
     @staticmethod
-    def open_file(file_name):
+    def get_db_connect(file_name):
             with open(file_name, "r") as f:
                 data_line = f.readlines()
-                return data_line[0].replace('\n', '')
+                db_name = data_line[0].replace("\n", "").split(":",1)[1]
+                username = data_line[1].replace("\n", "").split(":",1)[1]
+                host_name = data_line[2].replace("\n", "").split(":",1)[1]
+                password = data_line[3].replace("\n", "").split(":",1)[1]
+                db_connect = DbConnect(db_name, username, host_name, password)
+                return db_connect
 
     def connect_to_db(self):
         try:
-            conn_str = DbConnect.open_file('connection.csv')
-            conn = psycopg2.connect(conn_str)
-            conn.autocommit = True
+            self.connection = psycopg2.connect(self.conn_str)
+            self.connection.autocommit = True
             print("Connection successful!")
-            cursor = conn.cursor()
-            cursor.execute("""SELECT name FROM base_data WHERE name='Y-find'""")
-            return cursor.fetchall()
-        
         except Exception as e:
             print("Something went wrong. Unable to connect to the database. Check your login details!")
             print(e)
+
+    def query(self, query_str):
+        cursor = self.connection.cursor()
+        cursor.execute(query_str)
+        return cursor.fetchall()
+    
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from db_connection import DbConnect
 
-my_connection = DbConnect('connection.csv')
+my_connection = DbConnect.get_db_connect('connection.txt')
+my_connection.connect_to_db()
+answer = my_connection.query("""SELECT * FROM project""")
+print(answer)
+
